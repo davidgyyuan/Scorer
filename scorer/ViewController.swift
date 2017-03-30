0844f80b42376d8a0074659278cd48a4865629c4
David Yuan <davidyuan@utexas.edu>
2017-03-30 18:33:51 -0500

    Initial Commit

---
+++ scorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scorer
+//
+//  Created by David Yuan on 3/30/17.
+//  Copyright © 2017 David Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
