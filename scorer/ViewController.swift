8cb510e0057e3ad2be7f337a8a0336e58e876702
David Yuan <davidyuan@utexas.edu>
2017-04-03 00:56:40 -0500

    init

--- scorer/ViewController.swift
+++ scorer/ViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imageToUpload: UIImage? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func uploadButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(vc, animated:true, completion:nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        self.imageToUpload = editedImage
+        
+        self.dismiss(animated: true, completion: nil)
+        self.performSegue(withIdentifier: "d", sender: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! PhotoViewController
+        
+            vc.tempimage = imageToUpload
+        
     }
 
 
